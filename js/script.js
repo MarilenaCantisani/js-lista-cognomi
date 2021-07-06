e1e895d1e2efb8d0ad4768fc2326af4a349be081
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-07-06 15:14:19 +0200

    + Added file index.html / file script.js /

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*
+1. chiedi all’utente il cognome
+2. inseriscilo in un array con altri cognomi:
+‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+3. stampa la lista ordinata alfabeticamente sia in console che in pagina
+4. scrivi anche la posizione "umana" (partendo da 1) della lista
+in cui il nuovo utente si trova */
+
+var lastNames = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"]
+
+var userLastName = prompt("Inserisci qui il tuo cognome: ");
+console.log(lastName);
